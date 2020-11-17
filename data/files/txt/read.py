a6b2855385632f645a8d5c56959170425c66f9be
repl.it user <replituser@example.com>
2020-11-17 10:18:08 +0000

    Added code to demonstrate how to read a text file

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,4 +1,3 @@
-#aproach 1 using sead
 def search(file_name):
   print("Searching... ")
   with open(file_name) as file:
@@ -6,38 +5,6 @@ def search(file_name):
     for line in lines:
       print(f"Looked in {line}.")
     print("...Done!")
-def run():
-  search("data/files/txt/locations.txt")
-run()
-
-#aproach 2
-def search(file_name):
-  print("Searching... ")
-  with open(file_name) as file:
-    for line in file:
-      end_character = line[:-1]
-
-      if(end_character =='\n'):
-        print(f"Looked in {line[:-1]}.")
-      else:
-        print(f"Looked in {line}.")
-
-  print("...Done!")
-  
-def run():
-  search("data/files/txt/locations.txt")
-run()
-
-#aproach 3
-def search(file_name):
-  print("Searching... ")
-  with open(file_name) as file:
-    for line in file:
-      line = line.replace('\n', '')
-      print(f"Looked in {line}.")
-
-  print("...Done!")
-  
 def run():
   search("data/files/txt/locations.txt")
 run()
