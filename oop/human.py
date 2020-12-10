2f41ab187ee004a96e861d29ae79df0258bc0de7
repl.it user <replituser@example.com>
2020-12-10 18:12:34 +0000

    Added code to create basic human and robot classes

---
+++ oop/human.py
@@ -0,0 +1,18 @@
+class Human:
+  
+  # class Attribute
+  
+  MAX_ENERGY = 100
+  # A STATIC METHOD
+  def __init__(self, name = "Human", age = 0):
+    self.name = name
+    self.age = age
+    self.energy = Human.MAX_ENERGY
+  
+  def display(self):
+    print(f"I am {self.name}")
+  
+if (__name__ == "__main__"):
+  human = Human()
+  human.display()
+  
---
+++ oop/robot.py
@@ -0,0 +1,30 @@
+class Robot:
+
+  # A class attribute
+  laws = "Protect, Obey and Survive"
+   # class Attribute
+  
+  MAX_ENERGY = 100
+
+  # A static method
+
+  def the_laws():
+    print(Robot.laws)
+
+  
+  # An initialiser (special instance method)
+  def __init__(self, name = 'Robot', age = 0):
+
+    # An instance attribute
+    self.name = name
+    self.age = age
+    self.energy = Robot.MAX_ENERGY
+
+  # An instance method
+  def display(self):
+    print(f"I am {self.name}")
+  
+if (__name__ == "__main__"):
+  robot = Robot()
+  robot.display()
+  Robot.the_laws()
