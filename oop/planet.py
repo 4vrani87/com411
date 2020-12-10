215f351ebfe632f24e60ab0f12c7722b51b51d6f
repl.it user <replituser@example.com>
2020-12-10 21:01:52 +0000

    Updated planet class to a dictionary

--- oop/planet.py
+++ oop/planet.py
@@ -1,32 +1,36 @@
 from human import Human
 from robot import Robot
 
-
 class Planet:
+
   def __init__(self):
-    self.humans = []
-    self.robots = []
+    self.inhabitants = {
+      'humans':[],
+      'robots':[]
+    }
 
   def __repr__(self):
-    return f"planet(humans = {self.humans}, robots = {self.robots})"
+    return f"planet(humans={self.inhabitants['humans']}, robots={self.inhabitants['robots']})"
+
   def __str__(self):
-    return f"This planet has {len(self.humans)} humans and {len(self.robots)} robots"
+    return f"This planet has {len(self.inhabitants['humans'])} humans and {len(self.inhabitants['robots'])} robots."
+
   def add_human(self, human):
-    self.humans.append(human)
+    self.inhabitants['humans'].append(human)
+
   def add_robot(self, robot):
-    self.robots.append(robot)
+    self.inhabitants['robots'].append(robot)
 
   def remove_human(self, human):
-    self.humans.remove(human)
+    self.inhabitants['humans'].remove(human)
+
   def remove_robot(self, robot):
-    self.robots.remove(robot)
+    self.inhabitants['robots'].remove(robot)
 
 if (__name__ == "__main__"):
   planet = Planet()
-  #print(planet)
   print(repr(planet))
   prins = Human("Prins")
   planet.add_human(prins)
   print(repr(planet))
   print(planet)
-  
