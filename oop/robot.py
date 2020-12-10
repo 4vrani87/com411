a3f2853b57e6360ace83907726f9751b6bd5ef0c
repl.it user <replituser@example.com>
2020-12-10 19:19:40 +0000

    Updated classes human and robot with instace methods

--- oop/robot.py
+++ oop/robot.py
@@ -11,7 +11,6 @@ class Robot:
   def the_laws():
     print(Robot.laws)
 
-  
   # An initialiser (special instance method)
   def __init__(self, name = 'Robot', age = 0):
 
@@ -19,19 +18,49 @@ class Robot:
     self.name = name
     self.age = age
     self.energy = Robot.MAX_ENERGY
-
-  # An instance method
-  def display(self):
-    print(f"I am {self.name}")
+  
+  # magic methods
   def __repr__(self):
     return f'robot(name={self.name}, age={self.age}), energy = {self.energy}'
 
   def __str__(self):
     return f'Robot {self.name} is {self.age} years old and my energy is {self.energy}.'
   
+  # An instance methods
+  def display(self):
+    print(f"I am {self.name}")
+ 
+
+  def grow(self):
+    self.age += 1
+  def eat(self, amount):
+    potential_energy = self.energy + amount
+    if(potential_energy > Robot.MAX_ENERGY):
+      self.energy = Robot.MAX_ENERGY
+      return potential_energy - self.energy
+    else:
+      self.energy = potential_energy
+      return 0
+    def move(self, distance):
+      potential_energy = self.energy - distance
+      if (potential_energy < 0):
+        self.energy = 0 
+        return self.energy  - abs(potential_energy)
+      else:
+        self.energy = potential_energy
+        return 0
+
+
+  
 if (__name__ == "__main__"):
   robot = Robot()
   robot.display()
   Robot.the_laws()
   print(robot)
+  print(repr(robot))
+  robot.move(10)
+  print(repr(robot))
+  robot.eat(5)
+  print(repr(robot))
+  robot.eat(20)
   print(repr(robot))
